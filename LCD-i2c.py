6e87de315ac172cdf77b17132c938c91613bbcf0
Tijndagamer <tijndagamer25@gmail.com>
2016-06-29 10:41:34 +0200

    Add LCD-i2c.py LCD-i2c.py:     - Add LCD_i2c class     - Add constants     - Add __init__     - Add lcd_toggle_enable     - Add lcd_write_byte

---
+++ LCD-i2c.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+"""
+LCD-i2c.py
+    Python module to write to simple character screens with an i2c backpack.
+
+This program uses (modified) parts of lcd_i2c.py by Matt Hawkins, licensed under the
+GNU GPL v3 or at your option any later version.
+
+Copyright 2016 Tijndagamer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import smbus
+
+class LCD_i2c:
+
+    bus = smbus.SMBus(1)
+    address = None
+    lcd_width = None
+    lcd_max_lines = None
+    lcd_backlight = None
+
+    LCD_CHR = 1 # Mode - Sending data
+    LCD_CMD = 0 # Mode - Sending command
+
+    # Memory addresses for the lines
+    LCD_LINE_1_ADDRESS = 0x80
+    LCD_LINE_2_ADDRESS = 0xC0
+    LCD_LINE_3_ADDRESS = 0x94
+    LCD_LINE_4_ADDRESS = 0xD4
+
+    LCD_BACKLIGHT_ON = 0x08
+    LCD_BACKLIGHT_OFF = 0x00
+
+    ENABLE = 0b00000100 # Enable bit
+
+    # Timing constants
+    E_PULSE = 0.0005
+    E_DELAY = 0.0005
+
+    def __init__(self, address = 0x27, lcd_width = 16, lcd_max_lines = 2, lcd_backlight = True):
+        self.address = address
+        self.lcd_width = lcd_width
+        self.lcd_max_lines = lcd_max_lines
+        if lcd_backlight:
+            self.lcd_backlight = LCD_BACKLIGHT_ON
+        else:
+            self.lcd_backlight = LCD_BACKLIGHT_OFF
+
+        # Initialise display
+        lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+        lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+        lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+        lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+        lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+        lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+        time.sleep(E_DELAY)
+
+    def lcd_toggle_enable(self, byte):
+        """Enables toggle."""
+        time.sleep(self.E_DELAY)
+        bus.write_byte(self.address, (byte | ENABLE))
+
+    def lcd_write_byte(self, byte, mode):
+        """Send one byte of data to the i2c backpack.
+
+        byte: the data to send
+        mode: sending data or a command. 1 for data, 0 for a command.
+        """
+
+        byte_high = mode | (byte & 0xF0) | self.lcd_backlight
+        byte_low = mode | (byte << 4) & 0xF0 | self.lcd_backlight
