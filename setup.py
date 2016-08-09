8e6c5e27dc9957df6e6d465634aadb9f13264dd4
Tijndagamer <tijndagamer25@gmail.com>
2016-08-09 21:11:05 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setuptools
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='lcd_i2c-raspberrypi',
+    version='1.0',
+    description='A module to easily use a simple LCD with an i2c backpack with a raspberry pi',
+    classifiers=[
+        'Topic :: Software Development :: Libraries',
+    ],
+    keywords='raspberry pi raspberrypi LCD i2c',
+    url='https://github.com/Tijndagamer/lcd-i2c',
+    author='MrTijn/Tijndagamer',
+    author_email='tijndagamer25@gmail.com',
+    license='GPLv3',
+    packages=['lcd-i2c'],
+    install_requires=[
+        'smbus',
+    ],
+    scripts=['bin/lcd-i2c-example'],
+    zip_safe=False)
